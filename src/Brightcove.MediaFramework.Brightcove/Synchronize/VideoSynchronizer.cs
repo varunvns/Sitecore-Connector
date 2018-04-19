78c3457f63b69e1f4206bcd5fa607f3cac353f2e
Uy Tran <uytran@gmail.com>
2018-04-19 08:28:33 -0700

    BRIG-6

--- src/Brightcove.MediaFramework.Brightcove/Synchronize/VideoSynchronizer.cs
+++ src/Brightcove.MediaFramework.Brightcove/Synchronize/VideoSynchronizer.cs
@@ -29,7 +29,7 @@ namespace Brightcove.MediaFramework.Brightcove.Synchronize
                 item[Sitecore.MediaFramework.Brightcove.FieldIDs.MediaElement.Id] = video.Id;
                 item[Sitecore.MediaFramework.Brightcove.FieldIDs.MediaElement.Name] = video.Name;
                 item[Sitecore.MediaFramework.Brightcove.FieldIDs.MediaElement.ReferenceId] = video.ReferenceId;
-                item[Sitecore.MediaFramework.Brightcove.FieldIDs.MediaElement.ThumbnailUrl] = (video.Images != null && video.Images.Thumbnail != null) ? video.Images.Thumbnail.Src : null;
+                item[Sitecore.MediaFramework.Brightcove.FieldIDs.MediaElement.ThumbnailUrl] = (video.Images != null && video.Images.Thumbnail != null) ? video.Images.Thumbnail.Src?.Replace("http://", "https://") : null;
                 item[Sitecore.MediaFramework.Brightcove.FieldIDs.MediaElement.ShortDescription] = video.ShortDescription;
                 item[Sitecore.MediaFramework.Brightcove.FieldIDs.Video.CreationDate] = video.CreationDate.ToString();
                 item[Sitecore.MediaFramework.Brightcove.FieldIDs.Video.LongDescription] = video.LongDescription;
@@ -38,7 +38,7 @@ namespace Brightcove.MediaFramework.Brightcove.Synchronize
                 item[Sitecore.MediaFramework.Brightcove.FieldIDs.Video.Economics] = video.Economics.ToString();
                 item[Sitecore.MediaFramework.Brightcove.FieldIDs.Video.LinkUrl] = video.Link != null ? video.Link.URL : null;
                 item[Sitecore.MediaFramework.Brightcove.FieldIDs.Video.LinkText] = video.Link != null ? video.Link.Text : null;
-                item[Sitecore.MediaFramework.Brightcove.FieldIDs.Video.VideoStillUrl] = (video.Images != null && video.Images.Poster != null) ? video.Images.Poster.Src : null;
+                item[Sitecore.MediaFramework.Brightcove.FieldIDs.Video.VideoStillUrl] = (video.Images != null && video.Images.Poster != null) ? video.Images.Poster.Src?.Replace("http://", "https://") : null;
                 item[Sitecore.MediaFramework.Brightcove.FieldIDs.Video.CustomFields] = this.GetCustomFields(video);
                 item[FieldIDs.Video.Duration] = video.Duration.HasValue ? video.Duration.ToString() : string.Empty;
                 item[FieldIDs.Video.IngestJobId] = video.IngestJobId;
@@ -60,8 +60,8 @@ namespace Brightcove.MediaFramework.Brightcove.Synchronize
         {
             Video video = (Video)entity;
             VideoSearchResult videoSearchResult = (VideoSearchResult)searchResult;
-            var thumbnailSrc = video.Images.Thumbnail != null ? video.Images.Thumbnail.Src : null;
-            var posterSrc = video.Images.Poster != null ? video.Images.Poster.Src : null;
+            var thumbnailSrc = video.Images.Thumbnail != null ? video.Images.Thumbnail.Src?.Replace("http://", "https://") : null;
+            var posterSrc = video.Images.Poster != null ? video.Images.Poster.Src?.Replace("http://", "https://") : null;
             if (Sitecore.Integration.Common.Utils.StringUtil.EqualsIgnoreNullEmpty(video.LastModifiedDate.ToString(), videoSearchResult.LastModifiedDate) && (Sitecore.Integration.Common.Utils.StringUtil.EqualsIgnoreNullEmpty(thumbnailSrc, videoSearchResult.ThumbnailUrl)))
                 return !Sitecore.Integration.Common.Utils.StringUtil.EqualsIgnoreNullEmpty(posterSrc, videoSearchResult.VideoStillURL);
             return true;
