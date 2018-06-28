1c139b909caf48a78993e9d3f1282ff3ae336ce8
Steven Craft <steven.craft@xcentium.com>
2018-06-28 15:59:19 -0400

    BRIG-12
    Added code from v9.0 connector to v2.2 Media Framework connector.

--- src/Brightcove.MediaFramework.Brightcove/Constants.cs
+++ src/Brightcove.MediaFramework.Brightcove/Constants.cs
@@ -1,26 +1,58 @@
 ﻿namespace Brightcove.MediaFramework.Brightcove
 {
-    public static class Constants
-    {
-        public static readonly string BrightCoveOauthService = "mediaframework_brightcove_oauth";
-        public static readonly string BrightCoveCmsService = "mediaframework_brightcove_cms";
-        public static readonly string BrightCoveDynamicIngestService = "mediaframework_brightcove_dynamic_ingest";
+  public static class Constants
+  {
+    public static readonly string BrightCoveOauthService = "mediaframework_brightcove_oauth";
+    public static readonly string BrightCoveCmsService = "mediaframework_brightcove_cms";
+    public static readonly string BrightCoveDynamicIngestService = "mediaframework_brightcove_dynamic_ingest";
 
-        public static readonly string BrightcoveVideoCustomFieldsRouteName = "Sitecore.MediaFramework.Brightcove.Video.Customfields";
-        public static readonly string BrightcoveVideoTextTracksRouteName = "Sitecore.MediaFramework.Brightcove.Video.TextTracks";
-        public static readonly string BrightcoveFileDownloadRouteName = "Sitecore.MediaFramework.Brightcove.File.Download";
+    public static readonly string BrightcoveVideoCustomFieldsRouteName = "Sitecore.MediaFramework.Brightcove.Video.Customfields";
+    public static readonly string BrightcoveVideoTextTracksRouteName = "Sitecore.MediaFramework.Brightcove.Video.TextTracks";
+    public static readonly string BrightcoveFileDownloadRouteName = "Sitecore.MediaFramework.Brightcove.File.Download";
 
-        public static readonly string MvcDefaultRouteName = "Sitecore.MediaFramework.Brightcove.DefaultMvc";
-        public static readonly string WebApiDefaultRouteName = "Sitecore.MediaFramework.Brightcove.DefaultApi";
-        public static readonly string BrightcoveIngestionCallbackRouteName = "Sitecore.MediaFramework.Brightcove.Ingestion.Callback";
+    public static readonly string MvcDefaultRouteName = "Sitecore.MediaFramework.Brightcove.DefaultMvc";
+    public static readonly string WebApiDefaultRouteName = "Sitecore.MediaFramework.Brightcove.DefaultApi";
+    public static readonly string BrightcoveIngestionCallbackRouteName = "Sitecore.MediaFramework.Brightcove.Ingestion.Callback";
 
-        public static readonly string BrightcoveFileDownloadRouteTemplate = "/files/{fileId}";
+    public static readonly string BrightcoveFileDownloadRouteTemplate = "/files/{fileId}";
 
-        public static readonly string BrightcoveVideoCustomFieldsRouteTemplate = "/accounts/{accountItemId}/videos/{videoId}/customfields";
-        public static readonly string BrightcoveVideoTextTracksRouteTemplate = "/accounts/{accountItemId}/videos/{videoId}/texttracks";
+    public static readonly string BrightcoveVideoCustomFieldsRouteTemplate = "/accounts/{accountItemId}/videos/{videoId}/customfields";
+    public static readonly string BrightcoveVideoTextTracksRouteTemplate = "/accounts/{accountItemId}/videos/{videoId}/texttracks";
 
-        public static readonly string BrightcoveIngestionCallbackRouteTemplate = "/ingestioncallback/{operationId}";
+    public static readonly string BrightcoveIngestionCallbackRouteTemplate = "/ingestioncallback/{operationId}";
 
-        public static readonly string DefaultRouteTemplate = "/{controller}/{action}";
-    }
+    public static readonly string DefaultRouteTemplate = "/{controller}/{action}";
+    public static readonly string BrightcovePlaylistTemplateId = "{0E24292F-D7A5-4BA2-BCA0-CD5F14A89634}";
+
+    public static readonly string EmbedJavascript = "Javascript";
+    public static readonly string EmbedIframe = "iFrame";
+    public static readonly string SizingResponsive = "Responsive";
+    public static readonly string SizingFixed = "Fixed";
+  }
+
+  public static class BrightcovePlayerParameters
+  {
+    public static readonly string ItemId = "itemId";
+    public static readonly string Template = "template";
+    public static readonly string PlayerId = "ID";
+    public static readonly string Source = "source";
+    public static readonly string MediaId = "ID";
+    public static readonly string Width = "width";
+    public static readonly string Height = "height";
+    public static readonly string Autoplay = "autoplay";
+    public static readonly string Muted = "muted";
+    public static readonly string EmbedStyle = "embed";
+    public static readonly string Sizing = "sizing";
+    public static readonly string AspectRatio = "aspectRatio";
+    public static readonly string Ratio16X9 = "16:9";
+    public static readonly string Ratio4X3 = "4:3";
+    public static readonly string RatioCustom = "Custom";
+    public static readonly string PublisherId = "PublisherID";
+    public static readonly string PlaylistId = "ID";
+    public static readonly string PlaylistCreated = "CreationDate";
+    public static readonly string PlaylistUpdated = "LastModifiedDate";
+    public static readonly string PlaylistType = "PlaylistType";
+    public static readonly string AccountName = "AccountName";
+    public static readonly string ForceRender = "forceRender";
+  }
 }
