0a43a806e333dc69e6a16c1e898fa4e63cf193ba
Albert.Ong <albert.ong@xcentium.com>
2019-05-09 13:37:17 -0700

    commit brig-28 fixes

--- src/Sitecore.MediaFramework/ItemIDs.cs
+++ src/Sitecore.MediaFramework/ItemIDs.cs
@@ -14,7 +14,13 @@
 
     public static readonly ID UploadApplication = new ID("{E7C6251E-C323-4562-94CB-85FCF2BCB933}");
 
-    public static readonly ID EmbedMediaSublayout = new ID("{E393D906-954F-4442-A302-8029ED7A8EB0}");  
+    public static readonly ID EmbedMediaSublayout = new ID("{E393D906-954F-4442-A302-8029ED7A8EB0}");
+
+    public static readonly ID ClientId = new ID("{E7FCF624-4A36-4BE8-B2F5-D5C30BE677C4}");
+
+    public static readonly ID ClientSecret = new ID("{F537C3A0-152C-4AE1-A21B-EBF69B8EE51B}");
+
+    public static readonly ID PublisherId = new ID("{3B2A0B52-EEB1-42A9-989D-DD0F57986E67}");
 
     public static class PageEvents
     {
